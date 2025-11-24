122b7967bd32aba5e99c1dd22d9fe03dcbe1a170
DESKTOP-MVV37G8\ADMIN <minhnase173546@fpt.edu.vn>
2025-11-24 22:05:31 +0700

    [ADD] - update TypeScript configuration and CI/CD setup for NestJS monorepo

--- apps/backend/libs/shared-interceptor/src/lib/shared-interceptor.module.ts
+++ apps/backend/libs/shared-interceptor/src/lib/shared-interceptor.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
-import { RequestLoggingInterceptor } from './request-logging.interceptor';
-import { TransformInterceptor } from './tranform.interceptor';
+
 import { LoggingService } from './service/logging.service';
 
 @Module({
