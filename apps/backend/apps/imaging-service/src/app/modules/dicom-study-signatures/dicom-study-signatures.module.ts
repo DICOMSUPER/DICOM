54c2430554e282e4cf146f9c27529f5883cc6129
anhsapper13 <anhntse173567@fpt.edu.vn>
2025-12-03 17:17:22 +0700

    [FIX] - comment patientserviceclientmodule in signature dicom

--- apps/backend/apps/imaging-service/src/app/modules/dicom-study-signatures/dicom-study-signatures.module.ts
+++ apps/backend/apps/imaging-service/src/app/modules/dicom-study-signatures/dicom-study-signatures.module.ts
@@ -2,14 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { BackendEntitiesModule } from '@backend/database';
 import {
-  PatientServiceClientModule,
-  UserServiceClientModule,
+  UserServiceClientModule
 } from '@backend/shared-client';
 import { DicomStudy, DicomStudySignature } from '@backend/shared-domain';
-import { DicomStudySignaturesController } from './dicom-study-signatures.controller';
-import { DicomStudySignaturesService } from './dicom-study-signatures.service';
 import { ImageAnnotationsModule } from '../image-annotations/image-annotations.module';
 import { ImagingOrdersModule } from '../imaging-orders/imaging-orders.module';
+import { DicomStudySignaturesController } from './dicom-study-signatures.controller';
+import { DicomStudySignaturesService } from './dicom-study-signatures.service';
 
 @Module({
   imports: [
@@ -24,7 +23,7 @@ import { ImagingOrdersModule } from '../imaging-orders/imaging-orders.module';
   exports: [
     BackendEntitiesModule,
     UserServiceClientModule,
-    PatientServiceClientModule,
+    // PatientServiceClientModule,
   ],
 })
 export class DicomStudySignaturesModule {}
